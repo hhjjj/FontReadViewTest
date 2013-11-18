edd5bd15a904405b5295e3c90e4fe58403d354f9
hojun <songhojun@gmail.com>
2013-11-18 22:25:15 +0900

    first

---
+++ src/testApp.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxFontStash.h"
+#include "ofxNetwork.h"
+#include "ofxSyphon.h"
+
+//comment this line to ignore "ofxTimeMeasurements" calls
+//enabling it requires the ofxTimeMeasurements addon
+//https://github.com/armadillu/ofxTimeMeasurements
+//#define MEASURE_PERFORMANCE
+
+#ifdef MEASURE_PERFORMANCE
+#include "ofxTimeMeasurements.h"
+#else
+#define TIME_SAMPLE_START ;
+#define TIME_SAMPLE_STOP ;
+#define TIME_SAMPLE_DRAW_BOTTOM_RIGHT() ;
+#endif
+
+
+class testApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+        void drawPoint(float x, float y);
+        ofxTCPServer TCP;
+
+        ofxFontStash font;
+        ofxFontStash unicodeFont;
+        vector <string> msgLines;
+        vector <string> storeText;
+    vector <string> oneWords;
+	ofxSyphonServer mainOutputSyphonServer;
+
+};
